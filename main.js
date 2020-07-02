5fc63f193a53fd367bb12b7c4343546ef58ce422
Ivan <naviivan321@gmail.com>
2020-07-02 17:14:32 -0600

    finished implementing the menu in the mobile version

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 /*===========intro section=============*/
-var bouncingArrow = document.getElementById("bouncing-arrow");
-var mainWrapper = document.body;
-
-function disappearOnScroll(){
+function disappearOnScroll(bouncingArrow,mainWrapper){
     const scrollSupport = 'scrollBehavior' in document.documentElement.style;
     if ( !scrollSupport) {
         bouncingArrow.style["opacity"] = "0";
@@ -14,33 +11,34 @@ function disappearOnScroll(){
         }
     }
 }
-mainWrapper.addEventListener("scroll",disappearOnScroll);
+function loadIntro(){
+    var bouncingArrow = document.getElementById("bouncing-arrow");
+    var mainWrapper = document.body;
+    mainWrapper.addEventListener("scroll",function(){disappearOnScroll(bouncingArrow,mainWrapper)});
+}
 /*===========intro section end=============*/
 /*==========project section================*/
-var idGen = new IdGen();
-var id = idGen.getIdIncrement().toString();
-/*future projects will be stored as json files and will be called here*/
-const projects = [["Weather Calculator","https://github.com/navibryle/web-weather-app","web-weather-app/weather-home.html","weather_app.png",idGen.getIdIncrement().toString()],
-["Youtube Comment","https://github.com/navibryle/youtube-search-api","youtube-api/youtube-api/yt-api-home.html","yt-api.png",idGen.getIdIncrement().toString()]]
-for (var i = 0;i < projects.length;i++){
-    let aProject = new Project(projects[i][0],projects[i][1],projects[i][2],projects[i][3],projects[i][4]);
-    if (window.matchMedia("(pointer: coarse;)").matches){
-        aProject.displayProjectTap();
-        aProject.setTap();
-    }else{
-       aProject.displayProject();
-        aProject.setHover();
+function loadProjects(){
+    var idGen = new IdGen();
+    /*future projects will be stored as json files and will be called here*/
+    const projects = [["Weather Calculator","https://github.com/navibryle/web-weather-app","web-weather-app/weather-home.html","weather_app.png",idGen.getIdIncrement().toString()],
+    ["Youtube Comment","https://github.com/navibryle/youtube-search-api","youtube-api/youtube-api/yt-api-home.html","yt-api.png",idGen.getIdIncrement().toString()]]
+    for (var i = 0;i < projects.length;i++){
+        let aProject = new Project(projects[i][0],projects[i][1],projects[i][2],projects[i][3],projects[i][4]);
+        if (window.matchMedia("(pointer: coarse;)").matches){
+            aProject.displayProjectTap();
+            aProject.setTap();
+        }else{
+        aProject.displayProject();
+            aProject.setHover();
+        }
     }
 }
+
 /*==========project section end================*/
 /*==========contact button section ============*/
-var buttonWrapper = document.getElementById("contact-button-wrapper");
-var button = document.getElementById("contact-button");
-var contactHTML = `
-        <div id = "email">ipenales.dev@gmail.com</div>
-        <div>(403)913-5645</div>`
-var defaultHTML = "get in touch"
 function copyEmailToClip(){
+    //will copy my email to clipboard
     var email = document.createElement("textarea");
     document.body.appendChild(email);
     email.value = "ipenales.dev@gmail.com";
@@ -49,9 +47,7 @@ function copyEmailToClip(){
     document.body.removeChild(email);
 }
 function clipBoardNotif(){
-    const curWinTop = document.body.scrollTop;
-    const viewPortHeight = window.innerHeight;
-    const notifYPos = (viewPortHeight * 0.9) + curWinTop;
+    //
     var notif = document.createElement("span");
     const notifSpawn = document.getElementById("notif-spawn");
     notif.id = "notif";
@@ -60,7 +56,8 @@ function clipBoardNotif(){
     notifSpawn.appendChild(notif);
     window.setTimeout(function(){notifSpawn.removeChild(notif)},2*1000);
 }
-function spawnContact() {
+function spawnContact(button,defaultHTML,contactHTML) {
+    //button is where i am displaying the text instruction for user and defaultHTML is the 'get in touch' text and the contactHTML is the my email
     if (button.innerHTML == defaultHTML){
         button.innerHTML = contactHTML;
         copyEmailToClip()
@@ -69,13 +66,44 @@ function spawnContact() {
         button.innerHTML = defaultHTML;
     }
 }
-button.addEventListener("click",spawnContact);
-/*==========contact button section end=========*/
-//===================test======================
-function displayMenu(){
-    var menu = new Menu("menu-logo");
-    menu.displayLogo();
-    
+function loadContactButton(){
+    let button = document.getElementById("contact-button");
+    console.log(button);
+    let defaultHTML = "get in touch";
+    let contactHTML = `
+            <div id = "email">ipenales.dev@gmail.com</div>
+            <div>(403)913-5645</div>`;
+    button.addEventListener("click",function(){spawnContact(button,defaultHTML,contactHTML)});
 }
-displayMenu();
-//==================test end===================
+/*==========contact button section end=========*/
+//===================sticky/burger menu======================
+function displayCoarseMenu(){
+    var idGen = new IdGen();
+    var menu = new Menu("menu-logo");
+    menu.addContent("fab fa-github","https://github.com/navibryle",idGen.getIdIncrement().toString(),"Github");
+    menu.addContent("fab fa-hackerrank","https://www.hackerrank.com/naviivan321?hr_r=1",idGen.getIdIncrement().toString(),"Hackerrank");
+    menu.displayLogo();
+}
+function displayFineMenu(){
+    
+    var stickyParent = document.getElementById("sticky-parent");
+    stickyParent.innerHTML = `
+    <a href = "https://github.com/navibryle" target = "_blank"><span class="fab fa-github text-color logo-size hover-button"></span></a><br></br>
+    <a href = "https://www.hackerrank.com/naviivan321?hr_r=1" target = "_blank"><span class="fab fa-hackerrank text-color logo-size hover-button"></span></a>
+    `;
+}
+function loadMenu(){
+    if (matchMedia('(pointer:coarse').matches){
+        displayCoarseMenu();
+    }else{
+        displayFineMenu();
+    }
+}
+//==================sticky/burger menu end===================
+function main(){
+    loadIntro();
+    loadProjects();
+    loadContactButton();
+    loadMenu();
+}
+main();
--- menu.js
+++ menu.js
@@ -3,23 +3,31 @@ class Option{
     methods:
         displayOption();
     */
-   constructor(logoLink,link,id){
+   constructor(logoLink,link,id,text){
        this._logoLink = logoLink;
        this._link = link;
        this._id = id;
+        this._text = text;
    }
    getNode(){
     //will return an html node that will span the whole container
     //option css class will implement the onhover funcitonality
     let option = document.createElement('a');
     let img = document.createElement('span');
-    img.className = `this._logoLink logo-size`;
+    let text = document.createElement('span');
+    text.textContent = this._text;
+    text.className = "option-title";
+    img.className = `${this._logoLink} logo-size-menu`;
     option.href = this._link;
     option.target = "_blank";
     option.id = `${this._link}${this._id}`;
-    option.className = "option expand";
+    option.className = "option";
     option.appendChild(img);
+    option.appendChild(text);
     return option;
+   }
+   getNodeId(){
+       return `${this._link}${this._id}`;
    }
 }
 class Menu{
@@ -29,39 +37,33 @@ class Menu{
     constructor(menuLogo){
         //menuLogo is the id of the menuLogo and there will be a css selector that defines its position
         this._content = [];
-        this._menu = document.createElement("div");
+        this._menu = document.createElement("span");
         this._menuLogo = document.createElement("span");
         this._blackScreen = document.getElementById("black-screen");
-        this._menuLogo.id = this._menuLogoId;
+        this._menuLogo.id = menuLogo;
         this._menuLogo.className = "fas fa-bars menu-logo";
         this._menu.id = "vertical-nav";
         this._menu.className = "nav-activation-animation";
         this._isActive = false;
         this._navParentId = "nav-parent";
-        this._menuLogoId = menuLogo;
     }
-    addContent(logoLink,link,id){
-        var newOption = new Option(logoLink,link,id);
+    addContent(logoLink,link,id,text){
+        var newOption = new Option(logoLink,link,id,text);
         this._content.push(newOption);
     }
     isActive(){
         return this._isActive;
     }
-    initNav(){
-        const verticalNav = document.getElementById(this._menu.id);
+    navInit(){
         for (var i = 0;i<this._content.length;i++){
-            verticalNav.appendChild(this._content[i].getNode())
-        }
+            this._menu.appendChild(this._content[i].getNode());
+            }
     }
     activate(){
         /*
         screen turns black and navbar comesout successfully
         */
-       var parent = document.getElementById(this._navParentId);
-       for (var i = 0;i<this._content.length;i++){
-        this._menu.appendChild(this._content[i].getNode());
-        }
-        
+        var parent = document.getElementById(this._navParentId);
         this._blackScreen.classList.add("nav-black-screen-activate");
         parent.appendChild(this._menu);
         this._isActive = true;
@@ -72,11 +74,12 @@ class Menu{
         var parent = document.getElementById(this._navParentId);
         var child = this._menu;
         this._menu.classList.add("nav-deactivation-animation");
-        var object = this;
+        
         setTimeout(function(){
             parent.removeChild(child);
             object._menu.classList.remove("nav-deactivation-animation");
-        },150);
+        },200);
+        var object = this;
         this._isActive = false;
         this._blackScreen.classList.remove("nav-black-screen-activate");
         
@@ -103,10 +106,12 @@ class Menu{
         });
     }
     displayLogo(){
+        console.log("here");
         this.addMenuLogoListener();
         this.addBlackScreenListener();
+        this.navInit();
         const mainWrapper = document.getElementById("main-wrapper");
-        mainWrapper.appendChild(this._menuLogo);
+        mainWrapper.appendChild(this._menuLogo);   
     }
     
 }
